8c3b42b5aef923b4152e84320de3dcc10a247cfe
LiviuFTW <158779176+LiviuFTW@users.noreply.github.com>
2025-11-10 22:17:52 +0100

    Add files via upload

---
+++ invatarea.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Header for input/output
+
+int main() { // Main function
+    std::cout << "Primul program" << std::endl; // Output statement with   << std:endl;  to end the first line
+    std::cout << "Hai ca a mers bine ziua 1" << std::endl; //  ;  to end the sentence
+    return 0; // Return statement
+}
